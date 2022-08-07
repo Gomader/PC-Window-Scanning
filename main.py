f7f5dea07b92431d267dd4527ee0e42a614f24a4
gomader <gh970818@hotmail.com>
2022-08-07 22:20:01 +0800

    this file is for testing

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import scanning
+from cv2 import imread
+from cv2 import circle, imshow, waitKey
+
 if __name__ == '__main__':
-    pass
+    source = scanning.getScreenShot()
+    icon = imread("./testForScanning/12.png", 0)
+    center = scanning.imageMatching(source, icon)
+    img = circle(source, center, 5, (0,0,255), 1)
+    imshow('res', img)
+    waitKey(0)
